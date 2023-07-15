aef53dbd337fa6448f747da94ce3586311a50a46
marix-shiv <shivshankarrao696@gmail.com>
2023-07-15 13:36:13 +0530

    done

--- App.js
+++ App.js
@@ -1,1 +1,59 @@
+const saved = document.querySelector(".saved-news");
+const display = document.querySelector(".display");
 
+saved.addEventListener("click", function () {
+  // console.log("clicked");
+  const dupli = JSON.parse(localStorage.getItem("oldnews"));
+  if (dupli === null) {
+    display.innerHTML = `Your News Feed is Empty`
+ } else {
+    let jsonobj = dupli.map(JSON.stringify);
+    let uniq = new Set(jsonobj);
+    const data = Array.from(uniq).map(JSON.parse)
+    display.innerHTML = '';
+    const filterElements = document.createElement("div");
+    filterElements.className = 'newsStyle';
+    display.append(filterElements);
+    const filterData = data;
+    forSaved(filterElements, filterData);
+ }
+  // let news = null;
+  // if (duplicate !== null) {
+  //   let jsonObj = duplicate.map(JSON.stringify);
+  //   let unique = new Set(jsonObj);
+  //   news = Array.from(unique).map(JSON.parse);
+  //   for (let newsContent of news) {
+  //     const name = document.createElement("h2");
+  //     const div = document.createElement("div");
+  //     name.innerHTML = `By ${newsContent[" author"]}`;
+  //     div.className = "newsStyle"
+
+  //     const para = document.createElement("p");
+  //     para.innerHTML = `${newsContent.content} <a hrefa=${newsContent.url}>Read More...</a>`;
+
+  //     div.append(name);
+  //     div.append(para);
+  //     display.append(div)
+  //   }
+  // } else {
+  //   const para = document.createElement("p");
+  //   para.textContent = "No News Available";
+  //   display.append(para);
+  // }
+});
+function forSaved(parentElements, arr) {
+
+  //  console.log("onload");
+  //  display.innerHTML = "";
+   for (let news of arr) {
+      const h2 = document.createElement("h2");
+      const div = document.createElement("div");
+      div.className = "newsStyle"
+      h2.textContent = `By ${news[' author']}`;
+      const p = document.createElement("p");
+      p.innerHTML = `${news.content} <a href=${news.url}>Read more....</a>`;
+      div.append(h2);
+      div.append(p);
+      parentElements.appendChild(div);
+   }
+}
---
+++ newNews.js
@@ -0,0 +1,171 @@
+const display = document.querySelector(".display-contents")
+const business = document.querySelector("#business")
+const sports = document.querySelector("#sports");
+const world = document.querySelector("#world");
+const politics = document.querySelector("#politics");
+const hatke = document.querySelector("#hatke");
+const science = document.querySelector("#science");
+const myNewsFeed = document.querySelector(".my-newsFeed");
+const newNews = document.querySelector(".new-news");
+const clear = document.querySelector(".clear");
+let counter = 0;
+
+
+clear.addEventListener('click',function(){
+    localStorage.clear();
+    display.innerHTML = "";
+   //  console.log("Clicked");
+})
+
+async function getUser(){
+    let response = await fetch("https://content.newtonschool.co/v1/pr/64806cf8b7d605c99eecde47/news")
+    let data = await response.json();
+   //  console.log(data);
+    return data;
+}
+
+
+// getUser();
+function onloadData(parentElements, arr) {
+
+   //  console.log("onload");
+    parentElements.innerHTML = ""
+    for (let news of arr) {
+       const h2 = document.createElement("h2");
+       const div = document.createElement("div");
+       div.className = "newsStyle"
+       h2.textContent = `By ${news[' author']}`;
+       const p = document.createElement("p");
+       p.innerHTML = `${news.content} <a href=${news.url}>Read more....</a>`;
+       const btnElements = document.createElement("button");
+       btnElements.textContent = "Save News";
+       btnElements.addEventListener("click", function () {
+          btnElements.style.backgroundColor = "red";
+          const localData = JSON.parse(localStorage.getItem("oldnews"));
+          if (localData && localData.length > 0) {
+             localData.push(news);
+ 
+             localStorage.setItem("oldnews", JSON.stringify(localData));
+          } else {
+             localStorage.setItem("oldnews", JSON.stringify([news]));
+          }
+       });
+       div.append(h2);
+       div.append(p);
+       div.append(btnElements);
+       parentElements.appendChild(div);
+    }
+ }
+ function forSaved(parentElements, arr) {
+
+   //  console.log("onload");
+    parentElements.innerHTML = ""
+    for (let news of arr) {
+       const h2 = document.createElement("h2");
+       const div = document.createElement("div");
+       div.className = "newsStyle"
+       div.id = counter;
+       counter++;
+       h2.textContent = `By ${news[' author']}`;
+       const p = document.createElement("p");
+       const btnElements = document.createElement("button");
+       btnElements.textContent = "Delete News";
+       btnElements.addEventListener('click',()=>{
+         
+       })
+       p.innerHTML = `${news.content} <a href=${news.url}>Read more....</a>`;
+       div.append(h2);
+       div.append(p);
+       div.append(btnElements)
+       parentElements.appendChild(div);
+    }
+ }
+ myNewsFeed.addEventListener('click', function () {
+    const dupli = JSON.parse(localStorage.getItem("oldnews"))
+   //  console.log(dupli);
+    if (dupli === null) {
+       display.innerHTML = `Your News Feed is Empty`
+    } else {
+       let jsonobj = dupli.map(JSON.stringify);
+       let uniq = new Set(jsonobj);
+       const data = Array.from(uniq).map(JSON.parse)
+       display.innerHTML = '';
+       const filterElements = document.createElement("div");
+       filterElements.className = 'newsStyle';
+       display.append(filterElements);
+       const filterData = data;
+       forSaved(filterElements, filterData);
+    }
+ })
+ newNews.addEventListener('click', function () {
+    display.innerHTML = '';
+    const filterElements = document.createElement("div");
+    filterElements.className = 'newsStyle';
+    display.append(filterElements);
+    getUser().then((data) => {
+       const filterData = data;
+       onloadData(filterElements, filterData);
+    })
+})
+
+business.addEventListener('click', function () {
+   display.innerHTML = '';
+   const filterElements = document.createElement("div");
+   filterElements.className = 'newsStyle';
+   display.append(filterElements);
+   getUser().then((data) => {
+      const filterData = data.filter((news) => news[' category'] === 'business');
+      onloadData(filterElements, filterData);
+   })
+})
+sports.addEventListener('click', function () {
+    display.innerHTML = '';
+    const filterElements = document.createElement("div");
+    filterElements.className = 'newsStyle';
+    display.append(filterElements);
+    getUser().then((data) => {
+       const filterData = data.filter((news) => news[' category'] === 'sports');
+       onloadData(filterElements, filterData);
+    })
+ })
+ world.addEventListener('click', function () {
+    display.innerHTML = '';
+    const filterElements = document.createElement("div");
+    filterElements.className = 'newsStyle';
+    display.append(filterElements);
+    getUser().then((data) => {
+       const filterData = data.filter((news) => news[' category'] === 'world');
+       onloadData(filterElements, filterData);
+    })
+ })
+ politics.addEventListener('click', function () {
+   display.innerHTML = '';
+   const filterElements = document.createElement("div");
+   filterElements.className = 'newsStyle';
+   display.append(filterElements);
+   getUser().then((data) => {
+      const filterData = data.filter((news) => news[' category'] === 'politics');
+      onloadData(filterElements, filterData);
+   })
+})
+hatke.addEventListener('click', function () {
+   display.innerHTML = '';
+   const filterElements = document.createElement("div");
+   filterElements.className = 'newsStyle';
+   display.append(filterElements);
+   getUser().then((data) => {
+      const filterData = data.filter((news) => news[' category'] === 'hatke');
+      onloadData(filterElements, filterData);
+   })
+})
+science.addEventListener('click', function () {
+   display.innerHTML = '';
+   const filterElements = document.createElement("div");
+   filterElements.className = 'newsStyle';
+   display.append(filterElements);
+   getUser().then((data) => {
+      const filterData = data.filter((news) => news[' category'] === 'science');
+      onloadData(filterElements, filterData);
+   })
+})
+
